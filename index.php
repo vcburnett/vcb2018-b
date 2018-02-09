c3c33cf64de31a2e986e86c1ae801936cbeb8ff3
vcburnett <victor@konversion.ca>
2018-02-09 14:55:55 -0500

    Template Upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<!--<html lang="fr">-->
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1" name="viewport">
+	<!--<script src="//cdn.optimizely.com/js/{% code %}.js"></script>-->
+	<!-- Page metas -->
+	<!-- Social metas -->
+
+	<title>TITLE</title>
+	<link rel="icon" href="assets/images/favicon.png" type="image/x-icon"/>
+
+	<!-- stylesheets -->
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<!-- stylesheets -->
+
+	<!-- scripts -->
+</head>
+
+<body>
+
+</body>
+</html>
